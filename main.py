7d2712aac3bdfcf5261f1ee8d27d94908d195167
Vitalij <vitalijgl19@gmail.com>
2023-02-13 15:05:15 +0300

    fixed space machine

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class SpaceBeforeAndAfterWordsMachine(StateMachine):
         | got_first_operator.from_(got_space, cond = "is_operator")
         | got_first_operator.from_(got_alpha, got_digit,got_separator, cond = "is_operator", on = "make_space")#
         | got_second_operator.from_(got_first_operator, cond = "is_operator")
-        | got_space.from_(got_alpha, got_digit, got_separator, got_first_operator, got_second_operator, cond ="is_space")
+        | got_space.from_(got_alpha, got_digit, got_separator, got_first_operator, got_second_operator,got_space, cond ="is_space")
         | got_separator.from_(got_space, cond = "is_separator")
         | got_separator.from_(got_alpha, got_digit, got_separator, got_first_operator, got_second_operator, cond = "is_separator", on = "make_space")#
     )
@@ -39,36 +39,37 @@ class SpaceBeforeAndAfterWordsMachine(StateMachine):
         self.ans = ""
         super(SpaceBeforeAndAfterWordsMachine, self).__init__()
     def is_alpha(self, char):
-        return char.isalpha()
+        return (char.isalpha() or char == "\n")
     def is_digit(self, char):
-        return char.isdigit()
+        return (char.isdigit() or char == "\n")
     def is_dot(self, char):
         return char == '.'
     def is_operator(self, char):
         operators = ['<','>','=','+','-','*','/']
         return char in operators
     def is_separator(self, char):
-        separators = ['(',')','[',']','{','}','"',',',':']
+        separators = ['(',')','[',']','{','}','"',',',':',"'"]
         return char in separators
     def is_space(self, char):
         return char == ' '
     def make_space(self):
         self.ans += " "
-
     def after_symbols(self, char):
         self.ans += char
 
 
+t = SpaceBeforeAndAfterWordsMachine()
+for a in "a":
+    t.symbols(a)
+print(t.ans)
 
-testmachine = SpaceBeforeAndAfterWordsMachine()
-
-for s in "for i in range(50):":
-    testmachine.symbols(s)
-    print(testmachine)
-print(testmachine.ans)
-
-
-
-# with open('python_code.txt') as f:
-#     lines = f.readlines()
-#     print(lines)
+with open('python_code.txt') as f:
+    lines = f.readlines()
+    print(lines)
+    lines_with_spaces = []
+    for line in lines:
+        space_machine = SpaceBeforeAndAfterWordsMachine()
+        for char in line:
+            space_machine.symbols(char)
+        lines_with_spaces.append(space_machine.ans)
+    print(lines_with_spaces)
